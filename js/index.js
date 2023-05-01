44d01209dd9266e3db8cedb33b8e4a63cd831557
ifechukwudei <nwokoifechukwu@gmail.com>
2023-05-01 21:14:09 +0100

    correction

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
-window.addEventListener(
-  "load",
-  function () {
+window.addEventListener("load", function () {
+  // Hide the loading page after 15 seconds
+  setTimeout(function () {
     var loadingPage = document.getElementById("loading-page")
     loadingPage.style.display = "none"
-  },
-  15000
-)
+
+    // Show the content
+    var content = document.getElementById("content")
+    //content.style.display = "block"
+  }, 15000)
+})
